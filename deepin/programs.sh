50419be8be30df9bd265990775aedca67b7814a6
carlos3g <carlimbr156@gmail.com>
2020-03-25 23:33:45 -0300

    separando o download dos programas

---
+++ deepin/programs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -- baixando programas
+# - Telegram
+wget -P ~/Downloads/programs https://telegram.org/dl/desktop/linux
+# - Arduino IDE
+wget -P ~/Downloads/programs https://downloads.arduino.cc/arduino-1.8.12-linux64.tar.xz
+# - Natron
+wget -P ~/Downloads/programs https://github.com/NatronGitHub/Natron/releases/download/v2.3.14/Natron-2.3.14-Linux-x86_64bit.tgz
+# - WPS Office
+wget -P ~/Downloads/programs http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb
+# - Chrome
+wget -P ~/Downloads/programs https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
--- deepin/workspace.sh
+++ deepin/workspace.sh
@@ -70,17 +70,6 @@ apt-get -y install apt-transport-https
 apt-get -y update
 apt-get -y install code
 
-# -- baixando programas
-# - Telegram
-wget -P ~/Downloads/programs https://telegram.org/dl/desktop/linux
-# - Arduino IDE
-wget -P ~/Downloads/programs https://downloads.arduino.cc/arduino-1.8.12-linux64.tar.xz
-# - Natron
-wget -P ~/Downloads/programs https://github.com/NatronGitHub/Natron/releases/download/v2.3.14/Natron-2.3.14-Linux-x86_64bit.tgz
-# - WPS Office
-wget -P ~/Downloads/programs http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb
-# - Chrome
-wget -P ~/Downloads/programs https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 # -- configs
 # - zshrc
