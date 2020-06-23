cd4d09805450188c9171dab9082238d1492939d6
carlos3g <carlosmesquita156@gmail.com>
2020-06-23 16:23:26 -0300

    fixing

--- deepin/workspace.sh
+++ deepin/workspace.sh
@@ -1,78 +1,37 @@
 #!/bin/bash
+set -e
 
-apt -y update
-apt -y upgrade
-apt-get -y install wget
-apt-get -y install git-all
-
-# estrutura de pastas
-mkdir -p ~/Downloads/{ISOs, programs}
-mkdir -p ~/Documentos/programming/{js,python,arduino,open-source,projetos}
-mkdir -P ~/Android/Sdk
-
-# curl
-apt-get -y install curl
+# wget, curl e git
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+sudo apt-get install git -y
+sudo apt update
+sudo apt upgrade -y
 
 # python
-apt -y install python3 python3-pip
+sudo apt install python3 python3-pip python3-env idle3 -y
 
-# nvm, nodejs, npm e yarn
-apt-get -y install build-essential libssl-dev
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
+# estrutura de pastas
+mkdir -p ~/Downloads/ISOs
+mkdir -p ~/Downloads/programs
+mkdir -p ~/Documentos/programming/js
+mkdir -p ~/Documentos/programming/python
+mkdir -p ~/Documentos/programming/arduino
+mkdir -p ~/Documentos/programming/open-source
+mkdir -p ~/Documentos/programming/projetos
+mkdir -P ~/Android/Sdk
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get -y update && apt-get -y install yarn
-yarn config set -- --emoji true
+# programas
+./programs.sh
 
-# react, react-native-cli e expo-cli
-yarn global add create-react-app
-yarn global add react-native-cli
-yarn global add expo-cli
-apt-get -y install gcc-multilib lib32z1 lib32stdc++6
+# react e react-native
+./react-native.sh
 
-# jdk8
-add-apt-repository ppa:openjdk-r/ppa
-apt-get -y update
-apt-get -y install openjdk-8-jdk
-
-# configurando android jdk
-wget -P ~/Android/Sdk https://dl.google.com/android/repository/commandlinetools-linux-6200805_latest.zip -O commandlinetools-linux.zip
-unzip commandlinetools-linux.zip
-~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-27" "build-tools;27.0.3"
-
-# -- zsh
-apt -y install zsh
-chsh -s $(which zsh)
-# - oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# - fira code font
-add-apt-repository universe
-apt-get -y update
-apt -y install fonts-firacode
-# - spaceship
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
-ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
-# - plugins
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-
-# -- instalando alguns programas
-# - Visual Studio Code
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt-get -y install apt-transport-https
-apt-get -y update
-apt-get -y install code
-
+# zsh
+./zsh.sh
 
 # -- configs
 # - zshrc
-cp .zshrc ~
+mv ../.zshrc ~
 # - VS Code
-cp settings.json ~/.config/Code/User
+mv ../settings.json ~/.config/Code/User
