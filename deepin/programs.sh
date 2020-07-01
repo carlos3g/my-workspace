e013e51b6a3e98ddbae2072bf1b2af86f2ae4ef8
carlos3g <carlosmesquita156@gmail.com>
2020-07-01 10:24:17 -0300

    adicionando alguns programas

--- deepin/programs.sh
+++ deepin/programs.sh
@@ -3,14 +3,25 @@ set -e
 
 # - Telegram
 wget -c -P ~/Downloads/programs https://telegram.org/dl/desktop/linux
+
+# - Discord
+wget -c -P ~/Downloads/programs https://discordapp.com/api/download?platform=linux&format=deb
+
+# - Stacer
+wget -c -P ~/Downloads/programs https://github.com/oguzhaninan/Stacer/releases/download/v1.1.0/Stacer-1.1.0-x64.AppImage -O stacer.appimage
+
 # - Arduino IDE
 wget -c -P ~/Downloads/programs https://downloads.arduino.cc/arduino-1.8.12-linux64.tar.xz
+
 # - WPS Office
 wget -c -P ~/Downloads/programs http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/9126/wps-office_11.1.0.9126.XA_amd64.deb
+
 # - Chrome
 wget -c -P ~/Downloads/programs https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
 # - VS Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
