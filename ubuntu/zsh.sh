eb3f0fdcee6d18e2cb94a414c8f97d73c79f17b3
carlos3g <carlosmesquita156@gmail.com>
2020-10-17 19:49:05 -0300

    root permissions

--- ubuntu/zsh.sh
+++ ubuntu/zsh.sh
@@ -9,7 +9,7 @@ sudo add-apt-repository universe -y
 sudo apt-get update
 sudo apt install fonts-firacode -y
 # - spaceship
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
-ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
+sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
+sudo ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
 # - plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
