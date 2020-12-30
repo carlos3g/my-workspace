73222324c56d8aa31e788d10f237c977e44c552e
carlos3g <carlosmesquita156@gmail.com>
2020-12-30 19:32:18 -0300

    programas/...

--- ubuntu based/programs.sh
+++ ubuntu based/programs.sh
@@ -25,9 +25,6 @@ sudo apt-get install telegram -y
 # BalenaEtcher
 wget -c -P ~/Downloads/programs https://github.com/balena-io/etcher/releases/download/v1.5.100/balenaEtcher-1.5.100-x64.AppImage
 
-# - Discord
-wget -c -P ~/Downloads/programs https://discordapp.com/api/download?platform=linux&format=deb
-
 # - Stacer
 wget -c -P ~/Downloads/programs https://github.com/oguzhaninan/Stacer/releases/download/v1.1.0/Stacer-1.1.0-x64.AppImage
 
