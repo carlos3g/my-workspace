8115c832742d06c11de84f5513ddfe0018697fd1
carlos3g <carlosmesquita156@gmail.com>
2020-12-30 21:56:37 -0300

    debian/programs/discord

--- debian based/programs.sh
+++ debian based/programs.sh
@@ -10,8 +10,9 @@ sudo apt install woeusb -y
 # - BalenaEtcher
 wget -c -P ~/Downloads/programs https://github.com/balena-io/etcher/releases/download/v1.5.100/balenaEtcher-1.5.100-x64.AppImage
 
-# - Discord
-wget -c -P ~/Downloads/programs https://discordapp.com/api/download?platform=linux&format=deb
+# discord
+wget -c -O ~/Downloads/programs/discord.deb https://discord.com/api/download?platform=linux&format=deb
+sudo dpkg -i ~/Downloads/programs/discord.deb
 
 # - Stacer
 wget -c -P ~/Downloads/programs https://github.com/oguzhaninan/Stacer/releases/download/v1.1.0/Stacer-1.1.0-x64.AppImage
