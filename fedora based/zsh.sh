29c1b06f015afe470a4e05dd9b2f2a175f8d6ec4
carlos3g <carlosmesquita156@gmail.com>
2021-01-14 20:12:29 -0300

    [FEAT] add zsh.sh

---
+++ fedora based/zsh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# -- instalando zsh
+sudo dnf install zsh -y
+chsh -s $(which zsh)
+
+# - fira code font
+sudo dnf install fira-code-fonts -y
+
+# - oh-my-zsh
+# o oh-my-zsh deve ser executado manualmente, pois esse comando interrompe o script
+
+# - spaceship
+# o spaceship deve ser executado manualmente, pois esse comando interrompe o script
+
+# - plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
