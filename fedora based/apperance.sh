e9fa81fb00a7ce24a180cb09b64dd9b0e69654a0
carlos3g <carlosmesquita156@gmail.com>
2021-01-24 09:49:23 -0300

    [FEAT] add dracula-theme/gnome-terminal

--- fedora based/apperance.sh
+++ fedora based/apperance.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 set -e
+
+# - dracula-theme/gnome-terminal
+git clone https://github.com/dracula/gnome-terminal ~/Downloads/gnome-terminal
+cd ~/Downloads/gnome-terminal && ./install.sh
+rm -rf ~/Downloads/gnome-terminal
