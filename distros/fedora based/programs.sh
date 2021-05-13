2e688cd13d22ea012b04f76b3c7b9396631abfca
carlos3g <carlosmesquita156@gmail.com>
2021-05-13 10:08:22 -0300

    [REFACTOR] remove 'sudo dnf check-update'

--- distros/fedora based/programs.sh
+++ distros/fedora based/programs.sh
@@ -7,9 +7,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 
-sudo dnf check-update
-
-
 # - Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
