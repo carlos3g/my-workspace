a04793cf30950a9571cd456d139b0088a1e49f67
carlos3g <carlosmesquita156@gmail.com>
2021-05-13 10:15:27 -0300

    [FEAT] adds brave-install.fedora.sh util

---
+++ utils/brave-install.fedora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# this will download stable version of brave
+
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+sudo dnf install brave-browser
