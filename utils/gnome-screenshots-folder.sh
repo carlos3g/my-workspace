0510398cbd3a5b14aa866a92d1079ca43a2b0400
carlos3g <52337966+carlos3g@users.noreply.github.com>
2022-01-13 18:10:33 -0300

    [REFACTOR] improves comments

--- utils/gnome-screenshots-folder.sh
+++ utils/gnome-screenshots-folder.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+## This changes the screenshots folder to ~/Images/screenshots. Only works in gnome.
 set -e
-
-# this file will change the printscreens default folder to
-# /home/$USER/Images/screenshots
 
 mkdir ~/screenshots
 gsettings set org.gnome.gnome-screenshot auto-save-directory "file:///home/$USER/Images/screenshots"
 
-# After this, you'll need to change some shortcuts.
-# See https://askubuntu.com/questions/114429/how-can-i-specify-the-default-save-directory-for-gnome-screenshot
+# Now, You need to change some shortcuts. See https://askubuntu.com/a/961149
