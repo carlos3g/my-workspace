97490a4188bd51f16a87bfefb81ada9ec46df312
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2022-07-17 17:39:23 -0300

    refactor: adds vs code install to scripts

--- workspace-fedora.sh
+++ workspace-fedora.sh
@@ -27,7 +27,7 @@ sudo dnf install telegram -y
 # vs code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
+dnf check-update -y
 sudo dnf install code -y
 
 ### nodejs related
--- workspace-ubuntu.sh
+++ workspace-ubuntu.sh
@@ -7,15 +7,15 @@
 # - Installs some utils
 # - Installs Brave Browser
 # - Installs Telegram
+# - Installs VS Code
 # - Installs NodeJS and Yarn
 # - Installs expo-cli
 
 set -e
 
-sudo apt install git curl wget xclip g++ unrar unzip dconf-cli util-linux python3 python3-pip python3-dev -y
+sudo apt install git curl wget xclip g++ unrar unzip dconf-cli util-linux python3 python3-pip python3-dev gpg apt-transport-https -y
 
 # brave install
-sudo apt install apt-transport-https -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
@@ -25,6 +25,14 @@ sudo apt install brave-browser -y
 sudo add-apt-repository ppa:atareao/telegram
 sudo apt update -y
 sudo apt install telegram -y
+
+# vs code install
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update -y
+sudo apt install code -y
 
 ### nodejs related
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
