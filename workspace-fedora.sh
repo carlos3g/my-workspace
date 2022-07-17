e167ddf5aabe0b7d7d01b0866799856cf748bd66
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2022-07-17 18:14:58 -0300

    refactor: clean code

--- workspace-fedora.sh
+++ workspace-fedora.sh
@@ -13,30 +13,58 @@
 
 set -e
 
-sudo dnf check-update -y && sudo dnf upgrade -y
+update_system() {
+  sudo dnf check-update -y && sudo dnf upgrade -y
+}
 
-sudo dnf install git curl wget xclip g++ unzip dconf util-linux-user python3 python3-pip dnf-plugins-core -y
+install_utils() {
+  sudo dnf install git curl wget xclip g++ unzip dconf util-linux-user python3 python3-pip dnf-plugins-core -y
+}
 
-# brave install
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+install_brave() {
+  # See: https://brave.com/linux
+  sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-# telegram install
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install telegram -y
+  sudo dnf install brave-browser
+}
 
-# vs code install
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update -y
-sudo dnf install code -y
+install_telegram() {
+  # See: https://www.linuxcapable.com/how-to-install-telegram-on-fedora-36-linux
+  sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 
-# nodejs install
-curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-sudo dnf install nodejs -y
+  sudo dnf install telegram -y
+}
 
-# nodejs related installations
-sudo npm install --global yarn
-yarn config set -- --emoji true
-sudo yarn global add expo-cli -y
+install_vs_code() {
+  # See: https://code.visualstudio.com/docs/setup/linux
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+  dnf check-update -y
+  sudo dnf install code -y
+}
+
+install_nodejs() {
+  # See: https://github.com/nodesource/distributions/blob/master/README.md#deb
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
+
+  sudo dnf install nodejs -y
+}
+
+install_nodejs_related_programs() {
+  sudo npm install --global -y yarn
+
+  yarn config set -- --emoji true
+
+  sudo yarn global add expo-cli -y
+}
+
+update_system
+install_utils
+install_brave
+install_telegram
+install_vs_code
+install_nodejs
+install_nodejs_related_programs
--- workspace-ubuntu.sh
+++ workspace-ubuntu.sh
@@ -13,34 +13,63 @@
 
 set -e
 
-sudo apt update -y && sudo apt upgrade -y
+update_system() {
+  sudo apt update -y && sudo apt upgrade -y
+}
 
-sudo apt install git curl wget xclip g++ unrar unzip dconf-cli util-linux python3 python3-pip python3-dev gpg apt-transport-https -y
+install_utils() {
+  sudo apt install git curl wget xclip g++ unrar unzip dconf-cli util-linux python3 python3-pip python3-dev gpg apt-transport-https -y
+}
 
-# brave install
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install brave-browser -y
+install_brave() {
+  # See: https://brave.com/linux
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
-# telegram install
-sudo add-apt-repository ppa:atareao/telegram
-sudo apt update -y
-sudo apt install telegram -y
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-# vs code install
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt update -y
-sudo apt install code -y
+  sudo apt update -y
+  sudo apt install brave-browser -y
+}
 
-# nodejs install
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt install nodejs -y
+install_telegram() {
+  # See: https://www.edivaldobrito.com.br/telegram-no-ubuntu-fedora/
+  sudo add-apt-repository ppa:atareao/telegram
 
-# nodejs related installations
-sudo npm install --global yarn
-yarn config set -- --emoji true
-sudo yarn global add expo-cli -y
+  sudo apt update -y
+  sudo apt install telegram -y
+}
+
+install_vs_code() {
+  # See: https://code.visualstudio.com/docs/setup/linux
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings
+
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+
+  sudo apt update -y
+  sudo apt install code -y
+}
+
+install_nodejs() {
+  # See: https://github.com/nodesource/distributions/blob/master/README.md#deb
+  curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
+  sudo apt install nodejs -y
+}
+
+install_nodejs_related_programs() {
+  sudo npm install --global yarn
+  yarn config set -- --emoji true
+
+  sudo yarn global add expo-cli -y
+}
+
+update_system
+install_utils
+install_brave
+install_telegram
+install_vs_code
+install_nodejs
+install_nodejs_related_programs
