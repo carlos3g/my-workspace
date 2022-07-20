d81d9fac57a7539bb2aa6a60128a4152ce2f2c58
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2022-07-20 17:21:12 -0300

    fix: https://stackoverflow.com/q/41133455

--- utils/docker-install.ubuntu.sh
+++ utils/docker-install.ubuntu.sh
@@ -20,7 +20,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 sudo apt-get update -y
