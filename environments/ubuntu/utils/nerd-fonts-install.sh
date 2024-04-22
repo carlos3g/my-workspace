02dc17a7e2af3e6fa060dbd57f5dd5a640cce23a
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2024-04-22 20:50:03 -0300

    feat: adds references

--- environments/ubuntu/utils/nerd-fonts-install.sh
+++ environments/ubuntu/utils/nerd-fonts-install.sh
@@ -10,9 +10,12 @@
 #   - JetBrainsMono
 #   - Meslo
 
+# See: https://gist.github.com/matthewjberger/7dd7e079f282f8138a9dc3b045ebefa0?permalink_comment_id=4005789#gistcomment-4005789
+
 set -e
 
 function get_repo_last_release_version() {
+  # See: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8?permalink_comment_id=4843419#gistcomment-4843419
   basename $(curl -Ls -o /dev/null -w %{url_effective} https://github.com/ryanoasis/nerd-fonts/releases/latest)
 }
 
